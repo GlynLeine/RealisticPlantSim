e1639b72d8bb72565808832012ff11e9f8d5d37e
Robin Dittrich <462270@student.saxion.nl>
2021-05-21 10:09:36 +0200

    Autosave scene

--- RealisticPlantSim/Assets/Scripts/Procedural Terrain/TerrainUtilityEditor.cs
+++ RealisticPlantSim/Assets/Scripts/Procedural Terrain/TerrainUtilityEditor.cs
@@ -2,6 +2,7 @@ using System;
 using UnityEditor;
 using UnityEditor.SceneManagement;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 [CanEditMultipleObjects]
 [CustomEditor(typeof(TerrainUtility))]
@@ -96,9 +97,10 @@ public class TerrainUtilityEditor : Editor
 
         if (GUILayout.Button("Delete plants"))
         {
-            EditorSceneManager.MarkAllScenesDirty();
             plantGenerator.deleteAllPlants();
-
+            EditorSceneManager.MarkAllScenesDirty();
+            Scene scene = EditorSceneManager.GetActiveScene();
+            EditorSceneManager.SaveScene(scene);
         }
 
         GUILayout.EndHorizontal();
